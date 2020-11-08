6ec0e891bd81f33645605d1d6e1c8c4a720b79cb
gctev <gadgetscare.com@gmail.com>
2020-11-08 20:57:32 +0530

    Updates

---
+++ precache-manifest.611cde0638650a99b71ca5221a16baf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "121a3206513d9cc5c03f1656c358ee2d",
+    "url": "/sanmeet06/Expense-Tracker.git/index.html"
+  },
+  {
+    "revision": "89d8c28b65fecf19ebd7",
+    "url": "/sanmeet06/Expense-Tracker.git/static/css/main.82c2be50.chunk.css"
+  },
+  {
+    "revision": "6c54254ce418f575e48b",
+    "url": "/sanmeet06/Expense-Tracker.git/static/js/2.2740afcf.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/sanmeet06/Expense-Tracker.git/static/js/2.2740afcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89d8c28b65fecf19ebd7",
+    "url": "/sanmeet06/Expense-Tracker.git/static/js/main.8f90357c.chunk.js"
+  },
+  {
+    "revision": "1dbb66b566d44aa5705f",
+    "url": "/sanmeet06/Expense-Tracker.git/static/js/runtime-main.ce253b05.js"
+  }
+]);
